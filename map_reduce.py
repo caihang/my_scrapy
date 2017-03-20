505def7adc8077228f53213463a0d84953ad89a2
caihang <ch_snail@163.com>
2017-03-20 17:17:44 +0800

    add map reduce file

---
+++ map_reduce.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding:utf8 -*-
+
+import os
+import sys
+
+def reduce_func(x, y):
+    return x*10 + y
+
+if __name__ == '__main__':
+    init_list = [2, 4, 5, 6]
+    map_list = map((lambda x:x+2), init_list)
+    print 'map_list is:', map_list
+    reduce_res = reduce(reduce_func, map_list)
+    print 'reduce_res is:', reduce_res
